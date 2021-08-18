e816af0e1c42b60598f81ce50dc23c4b235d28d5
kaaviyah <kaaviyah7@gmail.com>
2021-08-18 16:25:31 +0530

    file added

---
+++ hello.js
@@ -0,0 +1,14 @@
+let num1=2
+let num2=5
+ let result=num1+num2
+ let num3=10
+console.log(result+num3)
+
+let username ="kaaviyah's acount'"
+console.log(username)
+
+let radius=5
+const pi=3.14
+let area=pi*radius*radius
+console.log(area)
+console.log(Number.MIN_VALUE*-10)
